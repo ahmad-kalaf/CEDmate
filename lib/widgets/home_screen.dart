d48ea942310e446389db3d4b7ed97afc96f8d666
ahmad <ahmad.it@outlook.de>
2025-10-30 22:42:06 +0100

    Add devtools_options.yaml and refactor home screen widgets for improved layout and functionality

--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -48,7 +48,64 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  Widget _buildKachel(String titel, IconData icon, VoidCallback onTap) {
+  Widget _iconZahlTextKachel(
+    IconData icon,
+    String titel,
+    int zahl,
+    VoidCallback onTap,
+  ) {
+    return Expanded(
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          height: 100,
+          margin: const EdgeInsets.symmetric(horizontal: 5),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Center(
+            // alles im Zentrum
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(icon, size: 30, color: Colors.grey),
+
+                const SizedBox(width: 10),
+
+                Column(
+                  mainAxisSize: MainAxisSize.min, // verhindert unnötige Höhe
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      zahl.toString(),
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      titel,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 30),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _iconTextKachel(String titel, IconData icon, VoidCallback onTap) {
     return Expanded(
       child: InkWell(
         onTap: onTap,
@@ -162,26 +219,41 @@ class _HomeScreenState extends State<HomeScreen> {
                         const SizedBox(height: 20),
                         Align(
                           alignment: Alignment.centerLeft,
-                          child: Text('+ Schnell erfassen'),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Icon(Icons.flash_on),
+                              SizedBox(width: 5),
+                              Text('Schnell erfassen'),
+                            ],
+                          ),
                         ),
                         const SizedBox(height: 10),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            _buildKachel('Symptom erfassen', Icons.sick, () {}),
-                            _buildKachel('Stuhlgang notieren', Icons.wc, () {}),
+                            _iconTextKachel(
+                              'Symptom erfassen',
+                              Icons.sick,
+                              () {},
+                            ),
+                            _iconTextKachel(
+                              'Stuhlgang notieren',
+                              Icons.wc,
+                              () {},
+                            ),
                           ],
                         ),
                         const SizedBox(height: 10),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            _buildKachel(
+                            _iconTextKachel(
                               'Mahlzeit Eintagen',
                               Icons.restaurant_menu,
                               () {},
                             ),
-                            _buildKachel(
+                            _iconTextKachel(
                               'Stimmung notieren',
                               Icons.mood,
                               () {},
@@ -189,16 +261,82 @@ class _HomeScreenState extends State<HomeScreen> {
                           ],
                         ),
                         const SizedBox(height: 20),
-                        ElevatedButton.icon(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => KalenderScreen(),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Icon(Icons.numbers),
+                              SizedBox(width: 5),
+                              Text('Heute in Zahlen'),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            _iconZahlTextKachel(
+                              Icons.sick,
+                              'Symptome heute',
+                              3,
+                              () {},
+                            ),
+                            _iconZahlTextKachel(
+                              Icons.wc,
+                              'Stuhlgänge',
+                              2,
+                              () {},
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            _iconZahlTextKachel(
+                              Icons.restaurant_menu,
+                              'Mahlzeiten',
+                              3,
+                              () {},
+                            ),
+                            _iconZahlTextKachel(
+                              Icons.mood,
+                              'Stimmung',
+                              2,
+                              () {},
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Icon(Icons.show_chart),
+                              SizedBox(width: 5),
+                              Text('Verschaffe dir einen Überblick'),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            _iconTextKachel(
+                              'Kalender',
+                              Icons.calendar_month,
+                              () => Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => KalenderScreen(),
+                                ),
                               ),
-                            );
-                          },
-                          label: Text('Zum Kalender'),
-                          icon: Icon(Icons.calendar_today),
+                            ),
+                            _iconTextKachel(
+                              'Statistiken',
+                              Icons.bar_chart,
+                              () {},
+                            ),
+                          ],
                         ),
                       ],
                     ),
