e31cec44ee35eeeecf64f15ec41d9adae89906d4
ahmad <ahmad.it@outlook.de>
2025-10-31 10:37:09 +0100

    Refactor home screen: Replace Column with Flexible for better layout handling and add overflow handling for text widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:cedmate/repositories/anamnese_repository.dart';
 import 'package:cedmate/services/anamnese_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // automatisch von `flutterfire configure` generiert
+import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
